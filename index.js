74d28a0472529c016b2d59f8751d5a48ca3e42a7
Frank Lin <franklin88@gmail.com>
2022-02-14 07:23:40 -0800

    Completed assignment

---
+++ index.js
@@ -0,0 +1,26 @@
+number = 10
+
+function add(x, y) {
+    return x + y
+}
+function subtract(x, y) {
+    return x - y
+}
+function multiply(x, y) {
+    return x * y
+}
+function divide (x, y) {
+    return x / y
+}
+function increment(x, y) {
+    return y = x + 1
+}
+function decrement(x, y) {
+    return y = x - 1
+}
+function makeInt(x) {
+    return parseInt(x, 10)
+}
+function preserveDecimal(x) {
+    return parseFloat(x, 10)
+}
